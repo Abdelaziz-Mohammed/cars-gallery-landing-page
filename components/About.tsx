39bf4306cdcde0f46924963a52215d3f33fd335f
Abdelaziz Mohamed <abdelazizmo2022@gmail.com>
2025-11-23 20:31:12 +0200

    commit

--- components/About.tsx
+++ components/About.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import Container from "./Container";
 
 export default function About() {
   return (
     <div className="bg-[#F6F6F6] py-20" id="about">
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <div className="relative h-full min-h-[400px] hidden md:block">
+      <div className="flex gap-0 relative">
+        <div className="relative h-full min-h-[435px] w-1/2 max-lg:hidden">
           <Image
             src={"/assets/images/about-img.png"}
             alt="About us"
@@ -12,29 +13,38 @@ export default function About() {
             className="object-cover w-full h-full"
           />
         </div>
+        <div className="h-full min-h-[435px] w-full lg:w-1/2"></div>
+        {/* text over image */}
         <div
-          className="px-8 sm:pr-12 md:pr-16 py-12 bg-center bg-no-repeat bg-cover bg-black/75 
-          bg-blend-color bg-[url('/assets/images/about-img.png')] md:bg-none md:bg-transparent"
+          className="absolute top-0 left-0 right-0 bottom-0
+          bg-no-repeat bg-cover bg-black/50 bg-blend-color max-h-[435px]
+          bg-[url('/assets/images/about-img.png')] lg:bg-none lg:bg-transparent"
         >
-          <h2 className="text-2xl sm:text-3xl md:text-4xl text-primary font-extrabold tracking-wide uppercase mb-6">
-            About Us
-          </h2>
-          <p className="text-sm sm:text-base text-white md:text-black mb-10">
-            Lorem ipsum dolor sit amet, consectetur adipis cing elit. Ornare
-            vestibulum diam sit adipis cing elit adipis cing elit.
-          </p>
-          <p className="text-sm sm:text-base text-white md:text-black mb-10">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas
-            ultrices scelerisque urna sed man proin lacinia. Posuere facilisis
-            ut nullam ipsum at enim. Ut imperdiet eu sodales eros.
-            <br />
-            nibh elementum eget. Integer dolor urna egestas sit donec neque, mi
-            elit. Diam praesent mi blandit uices vel amet. Sapien, ac tortor vel
-            ut pharetra, celerisqd. sklois kias rabs raffead book.
-          </p>
-          <button className="primary-btn w-full max-w-[200px]">
-            Read More
-          </button>
+          <Container className="flex gap-0">
+            <div className="max-lg:hidden w-1/2"></div>
+            <div className="w-full lg:w-1/2 lg:pl-10 max-h-full py-4">
+              <h2 className="text-2xl sm:text-3xl md:text-4xl text-primary font-extrabold tracking-wide uppercase mb-5">
+                About Us
+              </h2>
+              <p className="text-xs sm:text-sm text-white lg:text-black mb-8">
+                Lorem ipsum dolor sit amet, consectetur adipis cing elit. Ornare
+                vestibulum diam sit adipis cing elit adipis cing elit.
+              </p>
+              <p className="text-sm sm:text-base text-white lg:text-black mb-8">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas
+                ultrices scelerisque urna sed man proin lacinia. Posuere
+                facilisis ut nullam ipsum at enim. Ut imperdiet eu sodales eros.
+                <br />
+                nibh elementum eget. Integer dolor urna egestas sit donec neque,
+                mi elit. Diam praesent mi blandit uices vel amet. Sapien, ac
+                tortor vel ut pharetra, celerisqd. sklois kias rabs raffead
+                book.
+              </p>
+              <button className="primary-btn w-full max-w-[200px]">
+                Read More
+              </button>
+            </div>
+          </Container>
         </div>
       </div>
     </div>
